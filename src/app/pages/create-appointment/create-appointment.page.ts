c61aba4ab621a9d8262c68222d6deec746f64f0e
BenitaVelasco89 <benita.velasco.6121@gmail.com>
2025-11-26 12:41:21 -0600

    crear citas de parte del usuario
    archivos de crear citas de por parte del paciente

--- src/app/pages/create-appointment/create-appointment.page.ts
+++ src/app/pages/create-appointment/create-appointment.page.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton,
+  IonGrid, IonIcon, IonCard, IonRow, IonLabel, IonItem, IonRadio,
+  IonCol,IonList,IonSelect, IonSelectOption,IonText, IonInput , IonDatetime} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-create-appointment',
   templateUrl: './create-appointment.page.html',
   styleUrls: ['./create-appointment.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCol, IonRadio, IonItem, IonLabel, IonRow, IonCard,
+  IonIcon, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, 
+  CommonModule, FormsModule, IonButton,IonList,IonSelect, IonSelectOption,IonText, IonInput, IonDatetime]
 })
 export class CreateAppointmentPage implements OnInit {
+  nombreCita: string='';
+  apellidoCita: string='';
+  edadCita: string='';
+  correoCita: string= '';
+  medicoCita: any;
+  tipoCita: any;
+  fechahoraCita: string = new Date().toISOString(); 
+  
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  guardarCita(){
+
   }
 
 }

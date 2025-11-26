66d0d71fd5ad9945c1927749b0457563dfca9e4a
BenitaVelasco89 <benita.velasco.6121@gmail.com>
2025-11-26 12:39:25 -0600

    pagina de perfil del paciente
    cambios de la pagina de perfil del paciente

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,20 +1,115 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonAvatar, IonGrid,IonRow,
+    IonCol, IonItem, IonLabel, IonInput, IonButtons, IonModal, IonCard, IonRadio, 
+    IonRadioGroup, IonDatetime } from '@ionic/angular/standalone';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { Platform } from '@ionic/angular';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonDatetime, IonRadioGroup, IonRadio, IonCard, IonModal, IonButtons, IonButton,
+    IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonAvatar, IonGrid, 
+    IonRow, IonCol, IonItem, IonLabel, IonInput, CommonModule, FormsModule]
 })
 export class ProfilePage implements OnInit {
+  avatarUrl: string | undefined; 
+  nombre: string = '';
+  apellido:string ='';
+  edad:string ='';
+  generoSeleccionado: string = '';
+  peso: number = 0;
+  altura: number = 0;
+  correo: string = '';
+  //pdfUrl: string | null = null;   guarda la URL del PDF
+  mostrarModal = false;           // controla si el modal está abierto
+  pdfUrl: SafeResourceUrl | null = null;
+  
+  
 
-  constructor() { }
+  constructor(private platform: Platform, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
   }
 
+  async seleccionarImagen(){
+    if (this.platform.is('capacitor')){
+      try{
+        const image = await Camera.getPhoto({
+          quality: 90,
+          allowEditing: true, 
+          resultType: CameraResultType.Uri, 
+          source: CameraSource.Prompt,
+            promptLabelHeader:"Opciones",
+            promptLabelPhoto:"Galeria",
+            promptLabelPicture:"Camara",
+
+        });
+        this.avatarUrl = image.webPath;
+      }catch (error){
+        console.log("error con la imagen");
+      }
+    
+    } else{
+      const input =document.createElement('input');
+      input.type ='file';
+      input.accept = 'image/*';
+      input.onchange= (event: any) => {
+        const inputEl = event.target as HTMLInputElement;
+        if (inputEl?.files && inputEl.files.length > 0) {
+          const file = inputEl.files[0];
+          const reader = new FileReader();
+          reader.onload = () => {
+            this.avatarUrl = reader.result as string;
+          };
+          reader.readAsDataURL(file);
+        }
+      };
+      input.click();
+    }
+  }
+  quitarImagen() {
+  this.avatarUrl = undefined; // valor indefinido
 }
+
+  seleccionarPDF() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = 'application/pdf';
+    input.onchange = (event: any) => {
+      const inputEl = event.target as HTMLInputElement;
+      if (inputEl?.files && inputEl.files.length > 0) {
+        const file = inputEl.files[0];
+        const objectUrl = URL.createObjectURL(file); 
+        this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(objectUrl);// crea URL temporal del PDF
+      }
+    };
+    input.click();
+  }
+
+  abrirModal() {
+    this.mostrarModal = true;
+  }
+
+  cerrarModal() {
+    this.mostrarModal = false;
+  }
+
+  validarCorreo(){
+
+  }
+
+quitarExpediente() {
+  this.pdfUrl = null;   // limpia la variable
+  this.mostrarModal = false; // asegura que el modal esté cerrado
+}
+
+}
+
